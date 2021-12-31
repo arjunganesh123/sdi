5b1db82fba5f2db5b1f1cd8e6bc6e2e65770f60f
arjunganesh123 <arjunganeshgonibeedu44@gmail.com>
2021-12-31 11:34:31 +0530

    update widgetdesign

--- lib/constants/widgetdesign.dart
+++ lib/constants/widgetdesign.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 
-
+const klogindetails=InputDecoration(
+  hintText: '',
+  hintStyle: TextStyle(color: Colors.grey,fontSize: 18),
+  contentPadding: EdgeInsets.only(top: 20),
+  enabledBorder: UnderlineInputBorder(
+    borderSide: BorderSide(color: Colors.white,width: 1),
+  ),
+  focusedBorder: UnderlineInputBorder(
+    borderSide: BorderSide(color:Colors.blue,width: 3),
+  ),
+  border: UnderlineInputBorder(
+    borderSide: BorderSide(color: Colors.green,width: 2),
+  ),
+);
 
 const kuserdetails=InputDecoration(
   isDense: true,
